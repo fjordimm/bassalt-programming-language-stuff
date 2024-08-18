4a8d81c6122790339909e5fe572a6225b2ade412
fjordimm <fjordimm@gmail.com>
2024-08-18 14:25:37 -0700

    emptymessage

--- temp_and_testing/BenchmarkingAndRealTests/DependencyPointers/DependencyPointers.cpp
+++ temp_and_testing/BenchmarkingAndRealTests/DependencyPointers/DependencyPointers.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <cstdlib>
 #include <memory>
+#include <vector>
 
 // Forward declarations
 std::chrono::steady_clock::time_point makeClock();
@@ -12,6 +13,7 @@ std::size_t randNum(std::size_t lowerBound, std::size_t upperBound);
 void benchmarkRawPtr(const std::size_t numObjs, const std::size_t numActions);
 void benchmarkSharedPtr(const std::size_t numObjs, const std::size_t numActions);
 void benchmarkDependencyPtr(const std::size_t numObjs, const std::size_t numActions);
+void benchmarkDynamicDependencyPtr(const std::size_t numObjs, const std::size_t numActions);
 
 int main(void)
 {
@@ -259,6 +261,102 @@ void benchmarkDependencyPtr(const std::size_t numObjs, const std::size_t numActi
 
 				depobjects[indexDepobj] = objects[indexObj];
 				depobjects[indexDepobj]->dep = &(depobjects[indexObj]);
+			}
+			else if (actn == 1) // read the value from a random item of depobjects
+			{
+				std::size_t indexDepobj = randNum(0, numObjs - 1);
+
+				Object*& depobj = depobjects[indexDepobj];
+				if (depobj != nullptr)
+				{ float a = depobj->a; }
+			}
+		}
+
+		tActions = clockTime(clock);
+
+		for (std::size_t i = 0; i < numObjs; i++)
+		{
+			if (objects[i]->dep != nullptr)
+			{ *(objects[i]->dep) = nullptr; }
+			delete objects[i];
+		}
+
+		tDeletion = clockTime(clock);
+
+		delete[] objects;
+		delete[] depobjects;
+
+		tDeletionOfArrays = clockTime(clock);
+	}
+
+	std::printf("...Done\n");
+	std::printf("  Times:\n");
+	std::printf("    creation of array:     %f\n", tCreationOfArray);
+	std::printf("    object initialization: %f\n", tObjInitialization - tCreationOfArray);
+	std::printf("    actions:               %f\n", tActions - tObjInitialization);
+	std::printf("    deletion:              %f\n", tDeletion - tActions);
+	std::printf("    deletion of arrays:    %f\n", tDeletionOfArrays - tDeletion);
+	std::printf("  Memory:\n");
+	std::printf("    Object: %zub\n", sizeof(Object));
+	std::printf("    raw pointer: %zub\n", sizeof(Object*));
+}
+
+struct Object_dd
+{
+	float a;
+	float b;
+	float c;
+
+	std::vector<Object_dd*> deps;
+};
+
+void benchmarkDynamicDependencyPtr(const std::size_t numObjs, const std::size_t numActions)
+{
+	using Object = Object_dd;
+
+	std::printf("Benchmarking dynamic dependency pointers...\n");
+
+	auto clock = makeClock();
+	float tCreationOfArray;
+	float tObjInitialization;
+	float tActions;
+	float tDeletion;
+	float tDeletionOfArrays;
+
+	{
+		Object** objects = new Object*[numObjs];
+		Object** depobjects = new Object*[numObjs];
+		for (std::size_t i = 0; i < numObjs; i++)
+		{
+			objects[i] = nullptr;
+			depobjects[i] = nullptr;
+		}
+
+		tCreationOfArray = clockTime(clock);
+
+		for (std::size_t i = 0; i < numObjs; i++)
+		{
+			objects[i] = new Object();
+			objects[i]->a = (float)randNum(0, 100) / 100.0f;
+			objects[i]->b = (float)randNum(0, 100) / 100.0f;
+			objects[i]->c = (float)randNum(0, 100) / 100.0f;
+			// objects[i]->deps is also constructed
+		}
+
+		tObjInitialization = clockTime(clock);
+
+		for (std::size_t r = 0; r < numActions; r++)
+		{
+			int actn = randNum(0, 1);
+
+			if (actn == 0) // set a random item of depobjects to point to a random item of objects
+			{
+				std::size_t indexDepobj = randNum(0, numObjs - 1);
+				std::size_t indexObj = randNum(0, numObjs - 1);
+
+				depobjects[indexDepobj] = objects[indexObj];
+				depobjects[indexDepobj]->deps.
+				// depobjects[indexDepobj]->dep = &(depobjects[indexObj]);
 			}
 			else if (actn == 1) // read the value from a random item of depobjects
 			{

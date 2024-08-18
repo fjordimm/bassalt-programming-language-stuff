bb254df6c24e3cabbf73e3f9947569485ce0587c
fjordimm <fjordimm@gmail.com>
2024-08-18 13:26:21 -0700

    emptymessage

--- temp_and_testing/BenchmarkingAndRealTests/Dptr/Dptr.hpp
+++ temp_and_testing/BenchmarkingAndRealTests/Dptr/Dptr.hpp
@@ -1,4 +1,8 @@
 
+#include <cstdlib>
+#include <memory>
+#include <cstdio>
+
 template <typename T>
 class dptr;
 template <typename T>
@@ -7,15 +11,21 @@ class wdptr;
 template <typename T>
 class dptr
 {
-   private:
-	struct _dptr
-	{
-		T data;
-		wdptr* dep;
-	};
+	/* Structs */
 
    private:
-	_dptr* ptr;
+	struct tstruct
+	{
+		T data;
+		wdptr<T>* dep;
+	};
+	
+	/* Fields */
+
+   private:
+	tstruct* ptr;
+
+	/* Constructors */
 
    public:
 
@@ -26,19 +36,23 @@ class dptr
 		ptr(nullptr)
 	{}
 
-	dptr(dptr&& that) :
-		ptr(that.ptr)
-	{
-		that.ptr = nullptr;
-	}
-
-	dptr& operator=(nullptr_t null)
+	dptr& operator=(std::nullptr_t)
 	{
 		this->~dptr();
 
-		this->ptr = null;
+		this->ptr = nullptr;
 
 		return *this;
+	}
+
+	dptr(dptr&& that) :
+		ptr(nullptr)
+	{
+		this->~dptr();
+
+		this->ptr = that.ptr;
+
+		that.ptr = nullptr;
 	}
 
 	dptr& operator=(dptr&& that)
@@ -59,9 +73,9 @@ class dptr
 	{
 		if (this->ptr != nullptr)
 		{
-			if (this->ptr->weakPtr != nullptr)
+			if (this->ptr->dep != nullptr)
 			{
-				*(this->ptr->weakPtr) = nullptr;
+				*(this->ptr->dep) = nullptr;
 			}
 
 			delete this->ptr;
@@ -70,7 +84,7 @@ class dptr
 
 	static dptr make(T&& t)
 	{
-		return dptr();
+		return dptr(std::move(t));
 	}
 
    private:
@@ -78,8 +92,130 @@ class dptr
 	dptr(T&& t) :
 		ptr(nullptr)
 	{
-		this->ptr = new _dptr();
+		this->~dptr();
+
+		this->ptr = new tstruct();
 		this->ptr->data = t;
 		this->ptr->dep = nullptr;
+	}
+
+	/* Other Operator Overloads */
+
+   public:
+
+	T& operator*()
+	{
+		if (this->ptr == nullptr)
+		{
+			std::fprintf(stderr, "Null pointer.\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			return this->ptr->data;
+		}
+	}
+
+	/* Friends */
+
+	friend wdptr<T>;
+};
+
+template <typename T>
+class wdptr
+{
+	/* Fields */
+
+   private:
+	typename dptr<T>::tstruct* ptr;
+
+	/* Constructors */
+
+   public:
+
+	wdptr(const wdptr&) = delete;
+	wdptr& operator=(const wdptr&) = delete;
+
+	wdptr() :
+		ptr(nullptr)
+	{}
+
+	wdptr& operator=(std::nullptr_t)
+	{
+		this->~wdptr();
+
+		this->ptr = nullptr;
+
+		return *this;
+	}
+
+	wdptr(wdptr&& that) :
+		ptr(nullptr)
+	{
+		this->~wdptr();
+
+		this->ptr = that.ptr;
+		this->ptr->dep = this;
+
+		that.ptr = nullptr;
+	}
+
+	wdptr& operator=(wdptr&& that)
+	{
+		if (this != &that)
+		{
+			this->~wdptr();
+
+			this->ptr = that.ptr;
+			this->ptr->dep = this;
+
+			that.ptr = nullptr;
+		}
+
+		return *this;
+	}
+
+	~wdptr()
+	{
+		if (this->ptr != nullptr)
+		{
+			if (this->ptr->dep != nullptr)
+			{
+				this->ptr->dep = nullptr;
+			}
+		}
+	}
+
+	static wdptr from(dptr<T>& d)
+	{
+		return wdptr(d.ptr);
+	}
+
+   private:
+
+	wdptr(typename dptr<T>::tstruct* ptr) :
+		ptr(nullptr)
+	{
+		this->~wdptr();
+
+		this->ptr = ptr;
+		this->ptr->dep = this;
+	}
+
+	/* Other Operator Overloads */
+
+   public:
+
+	T& operator*()
+	{
+		if (this->ptr == nullptr)
+		{
+			std::fprintf(stderr, "Null pointer.\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			return this->ptr->data;
+		}
 	}
 };
--- temp_and_testing/BenchmarkingAndRealTests/Dptr/main.cpp
+++ temp_and_testing/BenchmarkingAndRealTests/Dptr/main.cpp
@@ -1,9 +1,22 @@
 
 #include <cstdio>
+#include "Dptr.hpp"
 
 int main(void)
 {
-	std::printf("Hoiwjeijwifj\n");
+	std::printf("Start of program\n");
+
+	dptr<float> d = dptr<float>::make(2.99832f);
+	std::printf("d has %f\n", *d);
+	wdptr<float> wd = wdptr<float>::from(d);
+	std::printf("d has %f\n", *d);
+	std::printf("wd has %f\n", *wd);
+	*d = 0.001f;
+	std::printf("d has %f\n", *d);
+	std::printf("wd has %f\n", *wd);
+	*wd = 111.888f;
+	std::printf("d has %f\n", *d);
+	std::printf("wd has %f\n", *wd);
 
 	return 0;
 }

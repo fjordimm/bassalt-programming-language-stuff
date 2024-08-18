63e19d90f83d290da1cd7aa6c00675c4acf1603e
fjordimm <fjordimm@gmail.com>
2024-08-18 09:02:11 -0700

    emptymessage

--- temp_and_testing/Benchmarking/DependencyPointers/DependencyPointers.cpp
+++ temp_and_testing/Benchmarking/DependencyPointers/DependencyPointers.cpp
@@ -1,42 +1,38 @@
 
 #include <cstdio>
 #include <chrono>
-
-// void Head::CalculateDeltaTime(float& deltaTime, float& totalTime)
-// {
-// 	using std::chrono::steady_clock;
-// 	using std::chrono::duration;
-// 	using durMillisecs = duration<float, std::ratio<1, 1000>>;
-
-// 	static steady_clock::time_point initialTimePoint = steady_clock::now();
-
-// 	static steady_clock::time_point timePoint = steady_clock::now();
-// 	steady_clock::time_point oldTimePoint = timePoint;
-// 	timePoint = steady_clock::now();
-// 	durMillisecs timeElapsedSinceLastFrame = std::chrono::duration_cast<durMillisecs>(timePoint - oldTimePoint);
-// 	deltaTime = timeElapsedSinceLastFrame.count();
-
-// 	durMillisecs timeElapsedSinceStart = std::chrono::duration_cast<durMillisecs>(timePoint - initialTimePoint);
-// 	totalTime = timeElapsedSinceStart.count();
-// }
+#include <random>
+#include <cstdlib>
 
 // Forward declarations
 std::chrono::steady_clock::time_point makeClock();
 float clockTime(const std::chrono::steady_clock::time_point& clock);
+std::size_t randNum(std::size_t lowerBound, std::size_t upperBound);
+void benchmarkRawPtr(const std::size_t n);
 
 int main(void)
 {
 	std::printf("yoooo what up world\n");
 
-	auto clock = makeClock();
-	for (int i = 0; i < 100000; i++)
+	for (int i = 0; i < 15; i++)
 	{
-		float z = ((float)i + 392.4) / 1.0000000012434;
+		std::printf("random number = %zu\n", randNum(1, 3));
 	}
-	float timeElapsed = clockTime(clock);
-	std::printf("time elapsed: %f\n", timeElapsed);
+
+	// auto clock = makeClock();
+	// for (int i = 0; i < 100000; i++)
+	// {
+	// 	float z = ((float)i + 392.4) / 1.0000000012434;
+	// }
+	// float timeElapsed = clockTime(clock);
+	// std::printf("time elapsed: %f\n", timeElapsed);
 
 	return 0;
+}
+
+void benchmarkRawPtr(const std::size_t n)
+{
+	std::printf("Benchmarking raw pointers...\n");
 }
 
 std::chrono::steady_clock::time_point makeClock()
@@ -57,4 +53,14 @@ float clockTime(const std::chrono::steady_clock::time_point& clock)
 	steady_clock::time_point newNow = steady_clock::now();
 	durMillisecs timeElapsedSinceLastFrame = std::chrono::duration_cast<durMillisecs>(newNow - clock);
 	return timeElapsedSinceLastFrame.count();
+}
+
+std::size_t randNum(std::size_t lowerBound, std::size_t upperBound)
+{
+	static long long seed = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count();
+	static std::default_random_engine gen(seed);
+	static std::uniform_int_distribution<std::size_t> dist(lowerBound, upperBound);
+	static std::size_t _ = dist(gen); // to make it go once
+	
+	return dist(gen);
 }
